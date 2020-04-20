1584e10f78e177d41b58e8f8e99f58d377085902
DAVIDE <davide.ferri.94@gmail.com>
2020-04-20 19:36:28 +0200

    Hierarchical linear reg with synthetic data, added

---
+++ HierLinReg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 19:32:05 2020
+
+@author: davideferri
+"""
+
+import logging 
+import numpy as np 
+import pandas as pd
+import scipy.stats as ss 
+import pymc3 as pm 
+import arviz as az
+
+s
