3f2adf3ae7f717d3d868c229ac2dcc5f51cf6be9
DAVIDE <davide.ferri.94@gmail.com>
2020-05-15 19:37:44 +0200

    Robust logistic regression example with Iris data, completed

--- RobustLogReg_IrisData.py
+++ RobustLogReg_IrisData.py
@@ -37,3 +37,56 @@ x_c = x_0 - x_0.mean()
 plt.figure()
 plt.plot(x_c,y_0,"o",color = "k")
 plt.show()
+
+# ---------------- specify a probabilistic model ---------------- # 
+
+with pm.Model() as model_rlg:
+    # set the priors for the parameters
+    alpha = pm.Normal("alpha",mu = 0, sd = 10)
+    beta = pm.Normal("beta",mu = 0, sd = 10)
+    mu = alpha + beta * x_c
+    # get the theta
+    theta = pm.Deterministic("theta", pm.math.sigmoid(mu))
+    # get the decision boundary 
+    db = pm.Deterministic("db", - alpha/beta)
+    # get a prior for the probability of an error
+    pie = pm.Beta("pie",1,1)
+    # finally get the probability of being of class 1
+    p = pie * 0.5 + (1 - pie) * theta
+    # specify the likelihood of the data
+    y_obs = pm.Bernoulli("y_obs", p = p, observed = y_0)
+    # inference step
+    trace = pm.sample(1500)
+    
+# ------------------- analyse the posterior distribution -------------- # 
+
+with model_rlg:
+    log.info("The summary on the trace is as follows: %s", az.summary(trace, var_names = ["alpha","beta","db","pie"]))
+    az.plot_trace(trace, var_names = ["alpha","beta","db","pie"])
+    
+# ------------------- plots ------------------------------------------- # 
+    
+# get the mean of theta
+theta_mean = trace["theta"].mean(0)
+# get idx
+idx = np.argsort(x_c)
+# plot the predicted p of the data
+plt.figure()
+plt.plot(x_c[idx],theta_mean[idx],color = "C2", lw = 3)
+# set a vertical line at the mean of the decision boundary 
+plt.vlines(trace["db"].mean(),0,1,color = "k")
+# get the hpd of db
+db_hpd = az.hpd(trace["db"])
+plt.fill_betweenx([0,1],db_hpd[0],db_hpd[1], color = "k", alpha = 0.5)
+plt.scatter(x_c,np.random.normal(y_0,0.02), marker='.', color=[f'C{x}' for x in y_0])
+az.plot_hpd(x_c,trace["theta"],color = "C2")
+plt.xlabel("petal length")
+plt.ylabel("theta",rotation=0)
+locs, _ = plt.xticks()
+plt.xticks(locs, np.round(locs + x_0.mean(), 1))
+plt.show()
+
+    
+    
+    
+    
